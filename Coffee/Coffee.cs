cb14143d56450461b1fb5d2598c56aefb55be114
Hemma Paramabuddhi <HemmaP@rthealthfund.com.au>
2018-06-22 14:55:26 +1000

    - init

---
+++ Coffee/Coffee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMachine
+{
+    public class Coffee
+    {
+        public bool IsLarge { get; set; }
+        public bool HasMilk { get; set; }
+        public int NumberOfSugar { get; set; }
+        public bool MilkIsFoamed { get; set; }
+        public bool HasSprinkles { get; set; }
+    }
+}
---
+++ Coffee/CoffeeCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMachine
+{
+    public class CoffeeCalculator
+    {
+        public double CalculateCost(Coffee coffee)
+        {
+            // Start with basic Expresso
+            double price = 1.50;
+
+            //Milk 25c or 30c for large
+            if (coffee.HasMilk && coffee.IsLarge)
+            {
+                price += .30;
+            }
+
+            if (coffee.HasMilk && !coffee.IsLarge)
+            {
+                price += .25;
+            }
+
+            // Calc sugar @ 10c each
+            price += (0.10 * coffee.NumberOfSugar);
+
+            // Milk Foaming 5c
+            if (coffee.MilkIsFoamed)
+            {
+                price += .05;
+            }
+
+            // Add service charge of $1.30 per order
+            price += 1.30;
+
+            return price;
+        }
+    }
+}
